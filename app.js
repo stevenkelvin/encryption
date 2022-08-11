fb8df604c9c172a729ea54b7f6cb0bdb34a28423
stevenkelvin <73333119+stevenkelvin@users.noreply.github.com>
2022-08-11 11:08:02 +0800

    Changed mongodb from local to online

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://stevenkelvin:XxuVuEP8vv6bDko8@cluster0.jtqkgzp.mongodb.net/", { useNewUrlParser: true });
 
 const userScheme = new mongoose.Schema({
     email: String,
